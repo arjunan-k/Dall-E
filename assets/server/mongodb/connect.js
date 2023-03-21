462e01615384c9d4b3df57a4301b4214964ac58f
Arjunan K <arjunank680620@gmail.com>
2023-03-21 16:52:49 +0530

    initial commit

---
+++ assets/server/mongodb/connect.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectDB = (url) => {
+  mongoose.set('strictQuery', true);
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('connected to mongo'))
+    .catch((err) => {
+      console.error('failed to connect with mongo');
+      console.error(err);
+    });
+};
+
+export default connectDB;
